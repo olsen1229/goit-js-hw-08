36c5a61fcd4034b6ff14d7fb68ca03d655602771
olsen1229 <marylou.olsen7@gmail.com>
2024-02-23 23:31:59 +0800

    initial commit

---
+++ src/js/03-feedback.js
@@ -0,0 +1,54 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('form.feedback-form');
+const emailEl = document.querySelector('label [name="email"]');
+const messageEl = document.querySelector('label [name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onForminput() {
+    const email = emailEl.value;
+    const message = messageEl.value;
+
+    const formData = {
+        email,
+        message,
+    };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+onPageReload();
+
+function onPageReload() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage) {
+        emailEl.value = savedMessage.email;
+        messageEl.value = savedMessage.message;
+    }
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const email = emailEl.value;
+    const message = messageEl.value;
+
+    if (email == '' || message == '') {
+        alert('Enter both input parameters!');
+        form.reset();
+        return;
+    }
+
+    const formData = { email, message };
+    console.log(formData);
+    form.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+  
